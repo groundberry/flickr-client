d5197ee9cdbd9f19f09c45d3c9e83d1a7602577b
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-03-29 15:49:20 -0700

    Remove unused constant and link

--- src/index.js
+++ src/index.js
@@ -5,13 +5,6 @@ import theme from './toolbox/theme'
 import App from './App';
 import './index.css';
 
-const imageUrls = [
-  '../img/golden_1.jpg',
-  '../img/golden_2.jpg',
-  '../img/golden_3.jpg',
-  '../img/golden_4.jpg'
-]
-
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <App imageUrls={imageUrls} />
